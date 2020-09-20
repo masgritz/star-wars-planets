42d222e2470ab5b7054834b66a996605d0a885a6
Matheus Gritz <matheusgritz@gmail.com>
2020-09-20 16:41:11 -0300

    Add cors and cross-env to dependencies

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
 const config = require('./utils/config')
 
 const planetRouter = require('./routers/planet.router')
@@ -14,6 +15,7 @@ mongoose.connect(config.MONGODB_URL, {
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 app.use('/api/planets', planetRouter)
 
 module.exports = app
